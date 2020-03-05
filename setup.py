bf6c2bd3f6438ba06ec433ba944d62fd7e79fa5f
kevinzakka <kevinarmandzakka@gmail.com>
2020-03-04 16:51:05 -0800

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
   name='torchnca',
-  version='0.1.0',
+  version='0.1.1',
   description='Neighbourhood Components Analysis in PyTorch.',
   long_description=long_description,
   long_description_content_type='text/markdown',
